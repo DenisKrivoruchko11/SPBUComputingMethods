a3b9d791ea0c342bbbfbe464345f18c89fde0a74
Denis Krivoruchko <denis.krvrchk@gmail.com>
2021-10-10 15:33:14 +0300

    add comments

--- src/main/homework2/task1/bfs.py
+++ src/main/homework2/task1/bfs.py
@@ -1,23 +1,32 @@
 from pygraphblas import Matrix, Vector, UINT8, BOOL, descriptor
 
 
+"""
+IN: 
+    m -- information about each edge as list of 3 lists. 
+    1st -- info about sources, 2nd -- info about targets, 3rd -- info about weights.
+    
+    start -- start vertex index.
+OUT:
+    v -- Vector, which contains level of vertex with number n at position with number n.
+"""
+
+
 def bfs(m, start):
+    def compute():
+        result = Vector.sparse(UINT8, matrix.nrows)
+        current = Vector.sparse(BOOL, matrix.nrows)
+        current[start] = True
+
+        for level in range(1, matrix.nrows + 1):
+            if not current.reduce_bool():
+                break
+            result.assign_scalar(level, mask=current)
+            result.vxm(matrix, mask=result, out=current, desc=descriptor.RC)
+
+        return result
+
     matrix = Matrix.from_lists(m[0], m[1], m[2])
-    result = Vector.sparse(UINT8, matrix.nrows)
-    q = Vector.sparse(BOOL, matrix.nrows)
 
-    q[start] = True
-    level = 1
+    return compute()
 
-    while q.reduce_bool() and level <= matrix.nrows:
-        result.assign_scalar(level, mask=q)
-        result.vxm(matrix, mask=result, out=q, desc=descriptor.RC)
-        level += 1
-
-    return result
-
-
-"""A = Matrix.from_lists(
-    [0, 0, 1, 3, 3, 4, 1, 5],
-    [1, 3, 2, 4, 5, 2, 5, 4],
-    [1, 1, 1, 1, 1, 1, 1, 1])"""
--- src/main/homework2/task1/triangles_count.py
+++ src/main/homework2/task1/triangles_count.py
@@ -1,6 +1,21 @@
 from pygraphblas import Matrix
 
 
+"""
+IN: 
+    m -- information about each edge as list of 3 lists. 
+    1st -- info about sources, 2nd -- info about targets, 3rd -- info about weights (only units allowed).
+OUT:
+    v -- count of triangles.
+"""
+
+
 def triangles_count(m):
-    matrix = Matrix.from_lists(m[0], m[1], m[2]).tril()
-    return matrix.mxm(matrix, mask=matrix).reduce_int()
+    def compute():
+        triangle_matrix = matrix.tril()
+
+        return triangle_matrix.mxm(triangle_matrix, mask=triangle_matrix).reduce_int()
+
+    matrix = Matrix.from_lists(m[0], m[1], m[2])
+
+    return compute()
